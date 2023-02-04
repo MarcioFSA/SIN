fc9ef15e6b1994f04af34302f892b36a429353e1
MarcioFSA <marcio.anderson.ti@gmail.com>
2023-02-03 21:56:57 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from view.principal import Ui_MainWindow
+# from ui.principal import Ui_MainWindow
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    Form.setWindowIcon(QtGui.QIcon('Logo.png'))
+    ui = Ui_MainWindow()
+    ui.setupUi(Form)
+
+    Form.show()
+    sys.exit(app.exec())
+
